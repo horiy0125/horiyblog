732d4cbb249939c3fd33f6132ca918afcadcd83e
horiy0125 <k.horiy0125@gmail.com>
2023-08-12 03:58:36 +0900

    feat: implement CareerSection

--- client/src/components/about/career.tsx
+++ client/src/components/about/career.tsx
@@ -6,7 +6,10 @@ type Props = {} & BaseProps;
 export const CareerSection = component$((props: Props) => {
   return (
     <section class={props.class}>
-      <h2>Career</h2>
+      <div class="headings">
+        <h2>Career</h2>
+        <p>経歴</p>
+      </div>
 
       <article>
         <h3>2019.4</h3>
@@ -15,7 +18,11 @@ export const CareerSection = component$((props: Props) => {
 
       <article>
         <h3>2019.10 - 2020.10</h3>
-        <h4>株式会社クアンド</h4>
+
+        <hgroup>
+          <h4>株式会社クアンド</h4>
+          <h5>エンジニア（長期インターン）</h5>
+        </hgroup>
 
         <p>
           Vue.js, DjangoでのFA（Factory Automation）Webシステム開発、Tensorflow,
@@ -29,7 +36,11 @@ export const CareerSection = component$((props: Props) => {
 
       <article>
         <h3>2020.11 - 2021.1</h3>
-        <h4>株式会社Regnio</h4>
+
+        <hgroup>
+          <h4>株式会社Regnio</h4>
+          <h5>エンジニア（長期インターン）</h5>
+        </hgroup>
 
         <p>
           Gatsby.jsでの自社HP開発やWebViewを利用したモバイルアプリの開発、Raspberry
@@ -39,8 +50,12 @@ export const CareerSection = component$((props: Props) => {
 
       <article>
         <h3>2021.2 - 2021.10</h3>
-        <h4>note株式会社</h4>
 
+        <hgroup>
+          <h4>note株式会社</h4>
+          <h5>web1チーム（旧カイゼンチーム）</h5>
+          <h6>エンジニア（長期インターン）</h6>
+        </hgroup>
         <p>
           Nuxt.js, Ruby on
           Railsを用いて、noteのフロントエンド・サーバサイド開発に従事。
@@ -50,6 +65,102 @@ export const CareerSection = component$((props: Props) => {
           ほかにも、Next.js,
           Svelteを用いてフロントエンドApp分割などにも携わった。
         </p>
+      </article>
+
+      <article>
+        <h3>2021.3.27 - 2021.3.28</h3>
+
+        <hgroup>
+          <h4>株式会社サイバーエージェント</h4>
+          <h5>Webフロントエンド</h5>
+          <h6>2days ハッカソン型インターン 3位入賞</h6>
+        </hgroup>
+
+        <p>
+          用意されたAPIを使い、2日間でECサイトを開発するハッカソン形式のインターン。
+        </p>
+        <p>
+          詳しくは
+          <a href="https://www.k-hori.com/blog/posts/311ze4r8ztg3">
+            ブログ記事
+          </a>
+          を参照。
+        </p>
+      </article>
+
+      <article>
+        <h3>2021.8.9 - 2021.8.27</h3>
+
+        <hgroup>
+          <h4>株式会社VOYAGE GROUP</h4>
+          <h5>Treasure 2021</h5>
+          <h6>グランプリ・UI/UX賞受賞</h6>
+        </hgroup>
+
+        <p>
+          詳しくは
+          <a href="https://www.k-hori.com/blog/posts/lsk8k8gpz">ブログ記事</a>
+          を参照。
+        </p>
+      </article>
+
+      <article>
+        <h3>2021.9.8</h3>
+
+        <hgroup>
+          <h4>クックパッド株式会社</h4>
+          <h5>Cookpad Summer Internship 2021</h5>
+          <h6>裏Techコース（1day）</h6>
+        </hgroup>
+
+        <p>
+          Next.js, Ruby on
+          Railsで構築されたアプリケーションを題材に、GraphQLを用いてAPIを開発するインターン。
+        </p>
+      </article>
+
+      <article>
+        <h3>2021.11.1 - 2021.12.9</h3>
+
+        <hgroup>
+          <h4>クックパッド株式会社</h4>
+          <h5>買物サービス開発部</h5>
+          <h6>エンジニア（就業型インターン）</h6>
+        </hgroup>
+
+        <p>
+          かいもの連携機能の開発に前半はSwiftを用いて（クックパッドiOSアプリ）、後半はNext.jsを用いて（Web版クックパッド）携わった。
+        </p>
+      </article>
+
+      <article>
+        <h3>2022.2 - 2022.9</h3>
+
+        <hgroup>
+          <h4>株式会社CARTA HOLDINGS</h4>
+          <h5>fluct アドプラットフォーム事業本部 開発本部</h5>
+          <h6>エンジニア（内定者アルバイト）</h6>
+        </hgroup>
+      </article>
+
+      <article>
+        <h3>2022.10 - 2023.3</h3>
+
+        <hgroup>
+          <h4>株式会社CARTA HOLDINGS</h4>
+          <h5>サポーターズ Tech Studio本部</h5>
+          <h6>エンジニア（内定者アルバイト）</h6>
+        </hgroup>
+      </article>
+
+      <article>
+        <h3>2023.4 - now</h3>
+
+        <hgroup>
+          <h4>株式会社CARTA HOLDINGS</h4>
+          <h5>サポーターズ Tech Studio本部</h5>
+          <h6>エンジニア</h6>
+        </hgroup>
       </article>
     </section>
   );
