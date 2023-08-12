5809e871b14263b4b191cf571ec921f4175e1aef
horiy0125 <k.horiy0125@gmail.com>
2023-08-12 20:28:59 +0900

    feat: add amam image

--- static/src/controllers/amamImage.ts
+++ static/src/controllers/amamImage.ts
@@ -5,10 +5,18 @@ export const AmamImageController = () => {
     ctx.response.status = Status.OK;
     ctx.response.type = "image/jpeg";
 
-    const path = new URL(
-      "../../images/20220509_153105.jpg",
-      import.meta.url,
-    );
+    const urls = [
+      new URL(
+        "../../images/20220509_153105.jpg",
+        import.meta.url,
+      ),
+      new URL(
+        "../../images/IMG_4836.jpg",
+        import.meta.url,
+      ),
+    ];
+
+    const path = urls[Math.floor(Math.random() * urls.length)];
     const file = await Deno.readFile(path);
 
     ctx.response.body = file;
