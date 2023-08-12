b9246cf334175f75d926f4d94badf4427ddd6f48
horiy0125 <k.horiy0125@gmail.com>
2023-08-12 20:11:46 +0900

    feat: implement ProfileSection

--- client/src/components/about/profile.tsx
+++ client/src/components/about/profile.tsx
@@ -14,68 +14,78 @@ export const ProfileSection = component$((props: Props) => {
 
       <article>
         <div class="grid">
-          <hgroup>
-            <h3>堀内 凱登（Kaito Horiuchi）</h3>
-            <h4>@horiy0125</h4>
-          </hgroup>
+          <div>
+            <hgroup>
+              <h3>堀内 凱登（Kaito Horiuchi）</h3>
+              <h4>@horiy0125</h4>
+            </hgroup>
+
+            <nav>
+              <ul>
+                <li>
+                  <Link
+                    isExternalLink
+                    openInNewTab
+                    href="https://github.com/horiy0125"
+                  >
+                    <img
+                      src="https://static.horiy.dev/favicon/github"
+                      width={32}
+                      height={32}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    isExternalLink
+                    openInNewTab
+                    href="https://twitter.com/horiy0125"
+                  >
+                    <img
+                      src="https://static.horiy.dev/favicon/twitter"
+                      width={32}
+                      height={32}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    isExternalLink
+                    openInNewTab
+                    href="https://qiita.com/horiy0125"
+                  >
+                    <img
+                      src="https://static.horiy.dev/favicon/qiita"
+                      width={32}
+                      height={32}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    isExternalLink
+                    openInNewTab
+                    href="https://note.com/horiy0125"
+                  >
+                    <img
+                      src="https://static.horiy.dev/favicon/note"
+                      width={32}
+                      height={32}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+
+            <h5>🐹</h5>
+          </div>
+
           <img
             src="https://static.horiy.dev/avatar/me"
             width={1600}
             height={1200}
           />
         </div>
-
-        <aside>
-          <nav>
-            <ul>
-              <li>
-                <Link
-                  isExternalLink
-                  openInNewTab
-                  href="https://atcoder.jp/users/horiy0125"
-                >
-                  AtCoder
-                </Link>
-              </li>
-              <li>
-                <Link
-                  isExternalLink
-                  openInNewTab
-                  href="https://github.com/horiy0125"
-                >
-                  GitHub
-                </Link>
-              </li>
-              <li>
-                <Link
-                  isExternalLink
-                  openInNewTab
-                  href="https://twitter.com/horiy0125"
-                >
-                  Twitter
-                </Link>
-              </li>
-              <li>
-                <Link
-                  isExternalLink
-                  openInNewTab
-                  href="https://qiita.com/horiy0125"
-                >
-                  Qiita
-                </Link>
-              </li>
-              <li>
-                <Link
-                  isExternalLink
-                  openInNewTab
-                  href="https://note.com/horiy0125"
-                >
-                  note
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </aside>
       </article>
     </section>
   );
