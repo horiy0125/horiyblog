8fa931982c821bbdc4f6e8130b46955e2dd0d7ad
horiy0125 <k.horiy0125@gmail.com>
2023-08-12 19:17:24 +0900

    feat: add favicon image routes to static

--- static/src/app.ts
+++ static/src/app.ts
@@ -1,11 +1,13 @@
 import { Application } from "./deps.ts";
 import healthCheck from "./routes/healthCheck.ts";
 import avatarImage from "./routes/avatarImage.ts";
+import faviconImage from "./routes/faviconImage.ts";
 
 const app = new Application();
 
 app.use(healthCheck.routes());
 
 app.use(avatarImage.routes());
+app.use(faviconImage.routes());
 
 await app.listen({ port: 8080 });
